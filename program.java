8be5d6752d5bde8d01663034e4efa49bad236846
1U23CA051KAVIN <139528325+1U23CA051KAVIN@users.noreply.github.com>
2024-12-12 09:44:55 +0000

    p1

---
+++ program.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class program
+{
+    public static void main(String args[])
+    {
+        System.out.println(x:"HELLO");
+        System.out.println(x:"a");
+        Scanner in = new Scanner(System.in);
+        int n = nextInt();
+        system.out.println(n);
+    }
+}
